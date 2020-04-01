45370d06ef30e9da1589ea845154e09fc4efcb99
gruoner <torsten@gruoner.ch>
2020-04-01 18:34:37 +0200

    Templates werden im Deployment angewendet

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ typeset PUB=/usr/lib/dart/bin/pub
 export https_proxy="http://10.230.1.1:8080"
 export no_proxy="127.0.0.1,localhost"
 typeset TARGETHOST
-[ "$2" == "dev" ] && TARGETHOST=devubuntu
-[ "$2" == "ref" ] && TARGETHOST=nightscout-stage
-[ "$2" == "prod" ] && TARGETHOST=nightscout
+typeset TEMPLATES2APPLY="lib/app_component.dart.template lib/src/globals.dart.template settings.json.template"
+[ "$2" == "dev" ] && TARGETHOST=devubuntu.home.local
+[ "$2" == "ref" ] && TARGETHOST=nightscout-stage.home.local
+[ "$2" == "prod" ] && TARGETHOST=nightscout.home.local
 if [ "$1" == "all" ]
 then
   echo "======================"
@@ -22,6 +23,10 @@ then
   echo "======================"
   echo "applying stage"
   echo "======================"
+  for TEMPLATE in $TEMPLATES2APPLY
+  do
+    sed -e"s/__NSR_HOST__/$TARGETHOST/g" < $TEMPLATE > $(echo $TEMPLATE | sed -e"s/.template//g")
+  done
   echo 
   echo "======================"
   echo "running build"
